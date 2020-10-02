f9398a2778495d11002e4abcbf0e08ca87773fe6
Roma <ChoporovRoman@gmail.com>
2020-10-02 18:59:08 +0300

    Now it works

---
+++ ViewModel.cs
@@ -0,0 +1,212 @@
+﻿using DevExpress.Mvvm;
+using HtmlAgilityPack;
+using NAudio.Wave;
+using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Media;
+using System.Net;
+using System.Security.Permissions;
+using System.Security.Policy;
+using System.Text.Encodings.Web;
+using System.Web;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace mp3player
+{
+    struct Data
+    {
+        private uint _index;
+        public uint index
+        {
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                _index = value;
+            }
+        }
+        private string _name;
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        private string _url;
+        public string url
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                _url = value;
+            }
+        }
+    }
+    class ViewModel : ViewModelBase
+    {
+        private BindingList<Data> _gridData = new BindingList<Data>();
+        private string _textBoxData = "";
+        private Data _selectedItem;
+        private MediaPlayer player = new MediaPlayer();
+        public Data selectedItem
+        {
+            get
+            {
+                return _selectedItem;
+            }
+            set
+            {
+                _selectedItem = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string textBoxData
+        {
+            get
+            {
+                return _textBoxData;
+            }
+            set
+            {
+                _textBoxData = value;
+                RaisePropertyChanged();
+            }
+        }
+        public BindingList<Data> gridData
+        {
+            get
+            {
+                return _gridData;
+            }
+            set
+            {
+                gridData = value;
+                RaisePropertyChanged(() => _gridData);
+            }
+        }
+        public ICommand playBtn
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    if (player.Source != null) player.Play();
+                });
+            }
+        }
+        public ICommand stopBtn
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    player.Stop();
+                });
+            }
+        }
+        public ICommand pauseBtn
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    if(player.CanPause) player.Pause();
+                });
+            }
+        }
+        public ICommand playTrackBtn
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    if (selectedItem.url == "") return;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://z1.fm");
+                    request.Credentials = CredentialCache.DefaultCredentials;
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    string cookie = response.Headers.Get("set-cookie");
+                    request = (HttpWebRequest)WebRequest.Create("https://z1.fm" + selectedItem.url);
+                    request.Headers.Add(HttpRequestHeader.Cookie, cookie);
+                    request.Method = "GET";
+                    request.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
+                    response = (HttpWebResponse)request.GetResponse();
+                    Uri uri = response.ResponseUri;
+                    string mp3 = uri.Scheme + "://" + uri.Host + uri.AbsolutePath;
+
+                    player.Open(new Uri(mp3));
+                    player.Play();
+                });
+            }
+        }
+        public ICommand btnClick
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    _gridData.Clear();
+                    Data trackBuf;
+                    if (textBoxData.Length == 0)
+                    {
+                        trackBuf = new Data();
+                        trackBuf.index = 0;
+                        trackBuf.name = "Wrong request";
+                        trackBuf.url = "";
+                        _gridData.Add(trackBuf);
+                        return;
+                    }
+                    //
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://z1.fm");
+                    request.Credentials = CredentialCache.DefaultCredentials;
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    string cookie = response.Headers.Get("set-cookie");
+                    //
+                    request = (HttpWebRequest)WebRequest.Create("https://z1.fm/mp3/search?keywords=" + textBoxData);
+                    request.Headers.Add(HttpRequestHeader.Cookie, cookie);
+                    response = (HttpWebResponse)request.GetResponse();
+                    StreamReader sr = new StreamReader(response.GetResponseStream());
+                    string str = sr.ReadToEnd();
+                    HtmlDocument document = new HtmlDocument();
+                    document.LoadHtml(str);
+                    HtmlNode body = document.GetElementbyId("container");
+                    HtmlNodeCollection tracks = body.SelectNodes(body.XPath + "//div[contains(@class, 'song song-xl')]");
+                    if (tracks == null)
+                    {
+                        trackBuf = new Data();
+                        trackBuf.index = 0;
+                        trackBuf.name = "Wrong request";
+                        trackBuf.url = "";
+                        _gridData.Add(trackBuf);
+                        return;
+                    }
+                    string name;
+                    for(int i = 0; i<tracks.Count;i++)
+                    {
+                        trackBuf = new Data();
+                        name = tracks[i].SelectSingleNode(tracks[i].XPath + "//div[contains(@class,'song-artist')]").LastChild.LastChild.InnerText;
+                        name = name + " ‒ " + tracks[i].SelectSingleNode(tracks[i].XPath + "//div[contains(@class, 'song-name')]").LastChild.LastChild.InnerText;
+                        trackBuf.url = tracks[i].SelectSingleNode(tracks[i].XPath + "//span[contains(@class, 'song-play btn4 play')]").GetAttributeValue("data-url", "Error!");
+                        trackBuf.name = HttpUtility.HtmlDecode(name);
+                        trackBuf.index = (uint)_gridData.Count + 1;
+                        _gridData.Add(trackBuf);
+                    }
+                });
+            }
+        }
+    }
+}
